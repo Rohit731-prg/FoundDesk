c74abfd5e81e31ef019f211b02e4c57cd1f825be
Rohit731-prg <rohitsingha821380@gmail.com>
2025-11-27 12:50:35 +0530

    Admin setup

---
+++ Client_Admin/src/App.tsx
@@ -0,0 +1,22 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* <Route path="/" element={<Login/>} />
+        <Route path="/signup" element={<Signup/>} /> */}
+
+        {/* Protected Routes */}
+        {/* <Route path="/home" element={ <ProtectedRoute><Home/></ProtectedRoute>} />
+        <Route path="/setting" element={ <ProtectedRoute><Seting/></ProtectedRoute>} />
+        <Route path="/profile" element={ <ProtectedRoute><Profile/></ProtectedRoute>} />
+        <Route path="/products" element={ <ProtectedRoute><Posts/></ProtectedRoute>} />
+        <Route path="/product" element={ <ProtectedRoute><Post/></ProtectedRoute>} /> */}
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
