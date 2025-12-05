3f0db0b370879adebbda4317f4fd3ef8e52ebddd
Rohit731-prg <rohitsingha821380@gmail.com>
2025-12-05 19:01:46 +0530

    working on redux error

--- Client_Admin/src/store/AdminSlice.js
+++ Client_Admin/src/store/AdminSlice.js
@@ -1,8 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
-import axiosInstance from "../Utils/axios";
-import { toast } from "sonner";
+import { loginFunction } from "./AdminThunk";
 
 const initialState = {
+  loading: true,
   admin: null,
   employees: [],
 };
@@ -10,29 +10,20 @@ const initialState = {
 export const adminSlice = createSlice({
   name: "admin",
   initialState,
-  reducers: {
-    login: async (state, action) => {
-      try {
-        const promise = axiosInstance.post("/admin/login", action.payload);
-
-        toast.promise(promise, {
-          loading: "Loading...",
-          success: (res) => res.data.message,
-          error: (err) => err?.response?.data?.message || "Login failed",
-        });
-
-        const response = await promise;
-        state.admin = response.data.admin;
-        console.log(state.admin);
-      } catch (error) {
-        console.log(error);
-      }
-    },
-
-    setEmployees: (state, action) => {
-      state.employees = action.payload;
-    },
-  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+           .addCase(loginFunction.fulfilled, (state, action) => {
+            state.loading = false;
+            state.admin = action.payload.admin;
+          })
+          .addCase(loginFunction.pending, (state) => {
+            state.loading = true;
+          })
+          .addCase(loginFunction.rejected, (state) => {
+            state.loading = false;
+          })
+  }
 });
 
 export const { login, setEmployees } = adminSlice.actions;
---
+++ Client_Admin/src/store/AdminThunk.js
@@ -0,0 +1,23 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axiosInstance from "../Utils/axios";
+import { toast } from "sonner";
+
+export const loginFunction = createAsyncThunk(
+    "admin/login",
+    async (data, { rejectWithValue }) => {
+        try {
+            const promise = axiosInstance.post("/admin/login", data);
+
+            toast.promise(promise, {
+                loading: "Loading...",
+                success: (res) => res.data.message,
+                error: (err) => err?.response?.data?.message || "Login failed",
+            });
+
+            const res = await promise;
+            return res.data;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+);
