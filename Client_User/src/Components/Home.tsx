84bba895043afa397fe6b889a4cbce3b215919ec
Rohit731-prg <rohitsingha821380@gmail.com>
2025-12-27 09:33:41 +0530

    user working

--- Client_User/src/Components/Home.tsx
+++ Client_User/src/Components/Home.tsx
@@ -1,6 +1,6 @@
 import useClaimStore from "../Store/ClaimStore";
-import useUserStore from "../Store/UserStore"
-import Navbar from "./Navbar"
+import useUserStore from "../Store/UserStore";
+import Navbar from "./Navbar";
 import { useEffect } from "react";
 import { FaLongArrowAltRight } from "react-icons/fa";
 
@@ -9,7 +9,7 @@ function Home() {
   const { claims, getAllClaim } = useClaimStore();
 
   useEffect(() => {
-    getAllClaim()
+    getAllClaim();
   }, []);
   return (
     <div className="p-5 ">
@@ -17,31 +17,35 @@ function Home() {
 
       <main>
         <p className="text-lg font-medium">Welcaome Back {user?.name}</p>
-        {claims ? claims == undefined ? (
-          <div>
-            <p>Server is not working now</p>
-          </div>
-        ) : (
-          <div>
-            {claims.map((claim) => (
-              <div>
+        {claims ? (
+          claims == undefined ? (
+            <div>
+              <p>Server is not working now</p>
+            </div>
+          ) : (
+            <div>
+              {claims.map((claim) => (
+                <div>
                   <p>{claim.claim_date}</p>
                   <img src={claim.item.image} alt="" />
-              </div>
-            ))}
-          </div>
+                </div>
+              ))}
+            </div>
+          )
         ) : (
           <div>
             <p>No Claims are made by you</p>
             <p>Check if you lost something !</p>
             <button>
-              <p>Check <FaLongArrowAltRight /></p> 
+              <p>
+                Check <FaLongArrowAltRight />
+              </p>
             </button>
           </div>
-        ) }
+        )}
       </main>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
